8e0290c22e9d77f387ec12fcca5107220db5b9fd
pbla2394@gmail.com <pbla2394@gmail.com>
2021-03-15 20:47:28 -0400

    Updated Cart total functionality and added dev comments

--- cart.js
+++ cart.js
@@ -1,65 +1,45 @@
 (function () {
-	// Start here
-	// let products = {
-	// 	lavendarMocha: {
-	// 		name: "Blended Lavender Mocha",
-	// 		price: 4.99,
-	// 	},
-	// 	brownSugar: {
-	// 		name: "Brown Sugar Maple Cinnamon Latte",
-	// 		price: 3.8,
-	// 	},
-	// 	coldBrew: {
-	// 		name: "Classic Cold Brew",
-	// 		price: 3.0,
-	// 	},
-	// 	saltedCaramel: {
-	// 		name: "Iced Salted Caramel Macchiato",
-	// 		price: 4.2,
-	// 	},
-	// 	dripCoffee: {
-	// 		name: "Drip Coffee",
-	// 		price: 2.7,
-	// 	},
-	// 	hotChocolate: {
-	// 		name: "Hot Chocolate",
-	// 		price: 3.29,
-	// 	},
-	// 	blondeVanilla: {
-	// 		name: "Blonde Vanilla Latte",
-	// 		price: 4.99,
-	// 	},
-	// 	darkChocolate: {
-	// 		name: "Dark Chocolate Mocha",
-	// 		price: 3.8,
-	// 	},
-	// 	bananaLoaf: {
-	// 		name: "Banana Nut Loaf",
-	// 		price: 1.99,
-	// 	},
-	// 	avocado: {
-	// 		name: "Avocado Toast",
-	// 		price: 4.49,
-	// 	},
-	// };
-	updateCartToal();
-
-	// targeting all remove buttons
-	let removeCartItemButtons = document.querySelectorAll(".remove");
-	console.log(removeCartItemButtons);
-
-	// looping through remove buttons and deleting the parent container when clicked
-	for (let i = 0; i < removeCartItemButtons.length; i++) {
-		let button = removeCartItemButtons[i];
-		button.addEventListener("click", function (event) {
-			var buttonClicked = event.target;
-			buttonClicked.parentElement.remove();
-			updateCartToal();
-		});
+	if (document.readyState == "loading") {
+		document.addEventListener("DOMContentLoaded", ready);
+	} else {
+		ready();
 	}
 
-	//
-	function updateCartToal() {
+	function ready() {
+		// targeting all remove buttons
+		let removeCartItemButtons = document.querySelectorAll(".remove");
+
+		// looping through remove buttons and deleting the parent container when clicked
+		for (let i = 0; i < removeCartItemButtons.length; i++) {
+			let button = removeCartItemButtons[i];
+			button.addEventListener("click", removeCartItem);
+		}
+		// targeting all quantity inputs
+		let quantity = document.getElementsByClassName("cartItemQuantity");
+
+		// looping through quantity inputs to account for quantity changes in the cart
+		for (let i = 0; i < quantity.length; i++) {
+			let input = quantity[i];
+			input.addEventListener("change", quantityChanged);
+		}
+	}
+	// removes the item's parent div when "remove" is clicked, and then update the cart total immediately
+	function removeCartItem(event) {
+		let buttonClicked = event.target;
+		buttonClicked.parentElement.remove();
+		updateCartTotal();
+	}
+
+	// makes sure the quantities changed are always a number and never a negative value
+	function quantityChanged(event) {
+		let input = event.target;
+		if (isNaN(input.value) || input.value <= 0) {
+			input.value = 1;
+		}
+		updateCartTotal();
+	}
+	// Updating the number of items and Subtotal
+	function updateCartTotal() {
 		let cartItemContainer = document.getElementsByClassName(
 			"cartContainer"
 		)[0];
@@ -67,6 +47,9 @@
 			"cartItem"
 		);
 		let total = 0;
+		let totalItems = 0;
+		let roundedTotal = 0;
+		// Loop through parent containers looking for price and quantity
 		for (let i = 0; i < cartItems.length - 1; i++) {
 			let cartItem = cartItems[i];
 			let priceElement = cartItem.getElementsByClassName(
@@ -75,17 +58,29 @@
 			let quantityElement = cartItem.getElementsByClassName(
 				"cartItemQuantity"
 			)[0];
-			var price = parseFloat(
+			// Removing the $ to make string an integer
+			let price = parseFloat(
 				priceElement.innerText.replace("$", "")
 			);
-			var quantity = quantityElement.value;
+			// Collecting all input values and setting a total amount of items
+			let quantity = quantityElement.value;
+			totalItems = parseInt(totalItems) + parseInt(quantity);
+
+			// Collecting all input values and setting a total amount of items
 			total = total + price * quantity;
-			console.log(total);
+			roundedTotal = total.toFixed(2);
+			console.log(roundedTotal);
 		}
+		// Replacing the HTML to hold the new total amount of items
+		let itemQty = document.getElementById("itemQty");
+		itemQty.innerText = `${totalItems} items`;
+
+		// Replacing the HTML to hold the new total price
 		let subtotal = document.querySelector(".totalPrice");
-		subtotal.innerText = "$" + total;
-		console.log(subtotal);
+		subtotal.innerText = "$" + roundedTotal;
+		console.log(subtotal, totalItems);
 	}
 
+	updateCartTotal();
 	// End here
 })();
