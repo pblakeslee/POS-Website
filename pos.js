bed2c1b158dfecab9bc74ab96724d15fe9520e2d
pbla2394@gmail.com <pbla2394@gmail.com>
2021-03-17 19:46:50 -0400

    added alert message for adding item to cart

--- pos.js
+++ pos.js
@@ -1,62 +1,62 @@
 (function () {
-  // Start here
+	// Start here
 
-  /* Event listeners for the catagory buttons 
+	/* Event listeners for the catagory buttons 
   Declaring all of the elements to target*/
-  const snackButton = document.getElementById("snacks");
-  const coldButton = document.getElementById("coldDrinks");
-  const hotButton = document.getElementById("hotDrinks");
-  const productWrapper = document.getElementById("productCards");
+	const snackButton = document.getElementById("snacks");
+	const coldButton = document.getElementById("coldDrinks");
+	const hotButton = document.getElementById("hotDrinks");
+	const productWrapper = document.getElementById("productCards");
 
-  //Creating the event listener for each button that is clicked//
+	//Creating the event listener for each button that is clicked//
 
-  if (snackButton !== null && snackButton !== undefined) {
-    snackButton.addEventListener("click", () => {
-      //looping through all the product cards//
-      for (let i = 0; i < productWrapper.children.length; i++) {
-        let product = productWrapper.children[i];
-        /*changing the visablity for each card if it contains the 
+	if (snackButton !== null && snackButton !== undefined) {
+		snackButton.addEventListener("click", () => {
+			//looping through all the product cards//
+			for (let i = 0; i < productWrapper.children.length; i++) {
+				let product = productWrapper.children[i];
+				/*changing the visablity for each card if it contains the 
 		  correct property that matches the button*/
-        if (product.classList.contains("snack")) {
-          product.style.display = "flex";
-        } else {
-          product.style.display = "none";
-        }
-      }
-    });
-  }
+				if (product.classList.contains("snack")) {
+					product.style.display = "flex";
+				} else {
+					product.style.display = "none";
+				}
+			}
+		});
+	}
 
-  if (coldButton !== null && coldButton !== undefined) {
-    coldButton.addEventListener("click", () => {
-      //looping through all the product cards//
-      for (let i = 0; i < productWrapper.children.length; i++) {
-        let product = productWrapper.children[i];
-        /*changing the visablity for each card if it contains the 
+	if (coldButton !== null && coldButton !== undefined) {
+		coldButton.addEventListener("click", () => {
+			//looping through all the product cards//
+			for (let i = 0; i < productWrapper.children.length; i++) {
+				let product = productWrapper.children[i];
+				/*changing the visablity for each card if it contains the 
 	  correct property that matches the button*/
-        if (product.classList.contains("cold")) {
-          product.style.display = "flex";
-        } else {
-          product.style.display = "none";
-        }
-      }
-    });
-  }
+				if (product.classList.contains("cold")) {
+					product.style.display = "flex";
+				} else {
+					product.style.display = "none";
+				}
+			}
+		});
+	}
 
-  if (hotButton !== null && hotButton !== undefined) {
-    hotButton.addEventListener("click", () => {
-      //looping through all the product cards//
-      for (let i = 0; i < productWrapper.children.length; i++) {
-        let product = productWrapper.children[i];
-        /*changing the visablity for each card if it contains the 
+	if (hotButton !== null && hotButton !== undefined) {
+		hotButton.addEventListener("click", () => {
+			//looping through all the product cards//
+			for (let i = 0; i < productWrapper.children.length; i++) {
+				let product = productWrapper.children[i];
+				/*changing the visablity for each card if it contains the 
 	  correct property that matches the button*/
-        if (product.classList.contains("hot")) {
-          product.style.display = "flex";
-        } else {
-          product.style.display = "none";
-        }
-      }
-    });
-  }
+				if (product.classList.contains("hot")) {
+					product.style.display = "flex";
+				} else {
+					product.style.display = "none";
+				}
+			}
+		});
+	}
 
-  // End here
+	// End here
 })();
