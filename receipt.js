eecaab35240df02d50c326c578bd1e9861459961
Ami Ross <amiross82792@gmail.com>
2021-03-17 19:20:43 -0400

    Added a clear cart when hitting back to menu

--- receipt.js
+++ receipt.js
@@ -1,22 +1,22 @@
 (function () {
-	// Start here
+  // Start here
 
-	//Get Cart
-	let cart = localStorage.getItem("cart");
-	cart = JSON.parse(cart);
+  //Get Cart
+  let cart = localStorage.getItem("cart");
+  cart = JSON.parse(cart);
 
-	//If cart is empty create array
-	if (cart === null) {
-		cart = [];
-	}
+  //If cart is empty create array
+  if (cart === null) {
+    cart = [];
+  }
 
-	//Items Container
-	let cartItmes = document.getElementById("miniReceiptItems");
+  //Items Container
+  let cartItmes = document.getElementById("miniReceiptItems");
 
-	//Loop thru cart and create items
-	cart.forEach((item, index) => {
-		if (item !== undefined) {
-			let htmlTemplate = `
+  //Loop thru cart and create items
+  cart.forEach((item, index) => {
+    if (item !== undefined) {
+      let htmlTemplate = `
             
             <div class="miniReceiptItem">
                                     <img
@@ -32,73 +32,73 @@
                                     <p class="receiptPrice">${item.price}</p>
                                 </div>`;
 
-			cartItmes.insertAdjacentHTML("beforeend", htmlTemplate);
-		}
-	});
+      cartItmes.insertAdjacentHTML("beforeend", htmlTemplate);
+    }
+  });
 
-	updateReceiptSubtotal();
+  updateReceiptSubtotal();
 
-	function updateReceiptSubtotal() {
-		let receiptItemContainer = document.getElementById(
-			"miniReceiptItems"
-		);
-		let receiptItems = receiptItemContainer.getElementsByClassName(
-			"miniReceiptItem"
-		);
-		let total = 0;
-		let totalItems = 0;
-		let roundedTotal = 0;
-		let tax = 0;
-		let roundedTax = 0;
-		let totalWithTax = 0;
-		let roundedTotalWithTax = 0;
+  function updateReceiptSubtotal() {
+    let receiptItemContainer = document.getElementById("miniReceiptItems");
+    let receiptItems = receiptItemContainer.getElementsByClassName(
+      "miniReceiptItem"
+    );
+    let total = 0;
+    let totalItems = 0;
+    let roundedTotal = 0;
+    let tax = 0;
+    let roundedTax = 0;
+    let totalWithTax = 0;
+    let roundedTotalWithTax = 0;
 
-		// Loop through parent containers looking for price and quantity
-		for (let i = 0; i < receiptItems.length; i++) {
-			let receiptItem = receiptItems[i];
-			let priceElement = receiptItem.getElementsByClassName(
-				"receiptPrice"
-			)[0];
+    // Loop through parent containers looking for price and quantity
+    for (let i = 0; i < receiptItems.length; i++) {
+      let receiptItem = receiptItems[i];
+      let priceElement = receiptItem.getElementsByClassName("receiptPrice")[0];
 
-			let quantityElement = receiptItem.getElementsByClassName(
-				"receiptItemQty"
-			)[0];
-			console.log(quantityElement);
+      let quantityElement = receiptItem.getElementsByClassName(
+        "receiptItemQty"
+      )[0];
+      console.log(quantityElement);
 
-			// Removing the $ to make string an integer
-			let price = parseFloat(
-				priceElement.innerText.replace("$", "")
-			);
+      // Removing the $ to make string an integer
+      let price = parseFloat(priceElement.innerText.replace("$", ""));
 
-			// Collecting all input values and setting a total amount of items
-			let quantity = parseInt(quantityElement.innerText);
-			totalItems = parseInt(totalItems) + parseInt(quantity);
+      // Collecting all input values and setting a total amount of items
+      let quantity = parseInt(quantityElement.innerText);
+      totalItems = parseInt(totalItems) + parseInt(quantity);
 
-			console.log(quantity, totalItems, price);
+      console.log(quantity, totalItems, price);
 
-			// Collecting all input values and setting a total amount of items
-			total = total + price * quantity;
-			roundedTotal = total.toFixed(2);
+      // Collecting all input values and setting a total amount of items
+      total = total + price * quantity;
+      roundedTotal = total.toFixed(2);
 
-			// Calculating Tax
-			tax = roundedTotal * 0.06;
-			roundedTax = tax.toFixed(2);
+      // Calculating Tax
+      tax = roundedTotal * 0.06;
+      roundedTax = tax.toFixed(2);
 
-			// Calculating Total With Tax
-			totalWithTax = roundedTotal * 1.06;
-			roundedTotalWithTax = totalWithTax.toFixed(2);
-		}
+      // Calculating Total With Tax
+      totalWithTax = roundedTotal * 1.06;
+      roundedTotalWithTax = totalWithTax.toFixed(2);
+    }
 
-		// Replacing the HTML to hold the new total price
-		let subtotal = document.getElementById("subtotalValue");
-		subtotal.innerText = "$" + roundedTotal;
+    // Replacing the HTML to hold the new total price
+    let subtotal = document.getElementById("subtotalValue");
+    subtotal.innerText = "$" + roundedTotal;
 
-		let taxValue = document.getElementById("taxValue");
-		taxValue.innerText = "$" + roundedTax;
+    let taxValue = document.getElementById("taxValue");
+    taxValue.innerText = "$" + roundedTax;
 
-		let totalValue = document.getElementById("totalValue");
-		totalValue.innerText = "$" + roundedTotalWithTax;
-	}
+    let totalValue = document.getElementById("totalValue");
+    totalValue.innerText = "$" + roundedTotalWithTax;
+  }
+  //Clearing the cart data when clicking the back to menu button
 
-	// End here
+  let cartClear = document.getElementsByClassName("back-button-link")[0];
+
+  cartClear.addEventListener("click", () => {
+    localStorage.clear();
+  });
+  // End here
 })();
